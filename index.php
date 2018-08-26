6796de0e04ed0c233911f84a88e51b013347eec1
atokad5 <kenny.krosky@gmail.com>
2018-08-25 19:39:25 -0700

    updates to fonts

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
 ?>
 
 
-site index
-
-test
+<div class="inner">
+	<h1>Im a h1</h1>
+	<h2>Im a h2</h2>
+	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse maxime saepe aut! Alias accusamus excepturi vero eum reprehenderit expedita facere inventore modi totam aperiam quaerat, doloremque libero saepe placeat tempore!</p>
+</div>
 
 
 <?php get_footer(); ?>
