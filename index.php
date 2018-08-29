997783ff8f02ff6868dc06f3d75f63896831c313
atokad5 <>
2018-08-29 07:22:35 -0700

    clean up

--- index.php
+++ index.php
@@ -4,19 +4,6 @@
 ?>
 
 
-<div class="inner">
-	<div class="spacing--md"></div>
-		<h1>Im a h1</h1>
-		<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse maxime saepe aut! Alias accusamus excepturi vero eum reprehenderit expedita facere inventore modi totam aperiam quaerat, doloremque libero saepe placeat tempore!</p>
-	<div class="spacing--sm"></div>
-</div>
 
-
-<div class="inner">
-	<div class="spacing--sm"></div>
-		<h1>Im a h1</h1>
-		<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse maxime saepe aut! Alias accusamus excepturi vero eum reprehenderit expedita facere inventore modi totam aperiam quaerat, doloremque libero saepe placeat tempore!</p>
-	<div class="spacing--sm"></div>
-</div>
 
 <?php get_footer(); ?>
