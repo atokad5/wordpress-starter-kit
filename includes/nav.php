4b27370798d190c1e5bbe4680a41af913d809a86
atokad5 <kenny.krosky@gmail.com>
2018-08-30 21:31:31 -0700

    removed partials

--- includes/nav.php
+++
@@ -1,6 +0,0 @@
-<li><a class="slide_link" href="#tickets">Tickets</a></li>
-<li><a class="lineup_link" href="#lineup">Lineup</a></li>
-<li><a class="slide_link" href="#venue">The Venue</a></li>
-<li><a class="slide_link" href="#location">Location</a></li>
-<li><a class="slide_link" href="#faqs">Faq's</a></li>
-<!-- <li><a href="mailto:support@bajabeachfest.com">Contact</a></li> -->
